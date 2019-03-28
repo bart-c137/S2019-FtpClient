f467d1630bb91019b7dab7f5cf692c25c21cf09a
bart-c137 <48188008+bart-c137@users.noreply.github.com>
2019-03-27 20:13:56 -0500

    The connect button pulls up the new connection window.

--- S2019-CMPSC390-FtpClient/src/com/sxu/bart/ftpclient/scenes/controllers/MainSceneController.java
+++ S2019-CMPSC390-FtpClient/src/com/sxu/bart/ftpclient/scenes/controllers/MainSceneController.java
@@ -9,9 +9,13 @@ import com.sxu.bart.ftpclient.ftp.FtpServer;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -30,34 +34,43 @@ public class MainSceneController {
     }    
     
     @FXML
-    private void tbbtnConnect_Click(ActionEvent e) {
-        String host = "speedtest.tele2.net";
-        int port = 21;
-        String user = "anonymous";
-        String password = "any";
+    private void tbbtnConnect_Click(ActionEvent e) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("../fxml/NewServerConnection.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("New Connection");
+        stage.show();
         
-        FtpServer server = new FtpServer();
-        server.setHost(host);
-        server.setPort(port);
-        server.setUser(user);
-        server.setPassword(password);
-        
-        try {
-            server.connect();
-            String[] fileNames = server.getFiles();
-            server.disconnect();
-            
-            Alert alert = new Alert(AlertType.INFORMATION);
-            StringBuilder buffer = new StringBuilder();
-            for(String file : fileNames) {
-                buffer.append(file);
-                buffer.append(System.getProperty("line.separator"));
-            }
-            alert.setContentText(buffer.toString());
-            alert.showAndWait();
-            
-        } catch (IOException ex) {
-            
-        }
+//        String host = "speedtest.tele2.net";
+//        int port = 21;
+//        String user = "anonymous";
+//        String password = "any";
+//        
+//        FtpServer server = new FtpServer();
+//        server.setHost(host);
+//        server.setPort(port);
+//        server.setUser(user);
+//        server.setPassword(password);
+//        
+//        try {
+//            server.connect();
+//            String[] fileNames = server.getFiles();
+//            server.disconnect();
+//            
+//            Alert alert = new Alert(AlertType.INFORMATION);
+//            StringBuilder buffer = new StringBuilder();
+//            for(String file : fileNames) {
+//                buffer.append(file);
+//                buffer.append(System.getProperty("line.separator"));
+//            }
+//            alert.setContentText(buffer.toString());
+//            alert.showAndWait();
+//            
+//        } catch (IOException ex) {
+//            
+//        }
     }
 }
